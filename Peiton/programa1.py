4c0d9bc39807ea09b10feb66def28e629a5d4a65
Nathan <nathan.consolo17@gmail.com>
2024-03-04 11:51:24 -0300

    Python
     fizemos o primeiro codigo em python

---
+++ Peiton/programa1.py
@@ -0,0 +1,16 @@
+print(" *-* fazendo programa")
+
+# Entrada de dados - Input()
+
+n1 = int(input("número 1: "))
+n2 = int(input("Número 2: "))
+
+# Processamento
+
+sub = n1-n2
+
+# Saída de dados - Output
+
+print("A Subtração de ", n1, "-", n2, " é ", sub)
+
+print("Seu programa, terminou \n Obrigado, mlk burro do krl, não sabe fazer continha de subtração kkk")
