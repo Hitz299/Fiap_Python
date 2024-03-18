bcc4eadde3e1351b99e70a5e9a5bca7454790d90
Nathan <nathan.consolo17@gmail.com>
2024-03-18 11:51:57 -0300

    atividade dhr de par e impar

--- Peiton/lacos-de-repeticao/while.py
+++ Peiton/lacos-de-repeticao/while.py
@@ -1,14 +1,14 @@
-i = int(input("Início: "))
-f = int(input("Fim: "))
 
-if (i < f):
-    while i <= f:
-        print(f"O valor atual de i: {i}")
+i = 1
+f = int(input("Número final: "))
+
+if (f > 0):
+    while i <= f: 
+        if(i % 2 == 0):
+            print(f"{i} é par")
+        elif (i % 2 == 1):
+            print(f"{i} é impar")
         i += 1
-        print(f"O fim é: {f}")
-        
-elif (i > f):
-    while i >= f:
-         print(f"O valor atual de i: {i}")
-         i -= 1
-         print(f"O fim é: {f}")
+
+else:
+    print("Entrada inválida")
