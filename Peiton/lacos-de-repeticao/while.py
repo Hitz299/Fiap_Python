ba459c659e4b568c6b8e565cb453657b701766b1
Nathan <nathan.consolo17@gmail.com>
2024-03-18 11:20:50 -0300

    Arrumei o codigo do while

--- Peiton/lacos-de-repeticao/while.py
+++ Peiton/lacos-de-repeticao/while.py
@@ -2,13 +2,13 @@ i = int(input("Início: "))
 f = int(input("Fim: "))
 
 if (i < f):
-    while i < f:
+    while i <= f:
         print(f"O valor atual de i: {i}")
         i += 1
         print(f"O fim é: {f}")
         
 elif (i > f):
-    while i > f:
+    while i >= f:
          print(f"O valor atual de i: {i}")
          i -= 1
          print(f"O fim é: {f}")
